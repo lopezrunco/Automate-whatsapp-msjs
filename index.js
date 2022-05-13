722aeabad6a7339982272417e356f88b1bd46dbd
Damián López <lopezrunco@gmail.com>
2022-05-13 16:37:29 +0530

    Sending msgs

---
+++ index.js
@@ -0,0 +1,23 @@
+const qrcode = require('qrcode-terminal')
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({ authStrategy: new LocalAuth() })
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+})
+client.on('ready', () => {
+    console.log('Client is ready!')
+    client.getChats().then(chats => {
+        const myGroup = chats.find(
+            (chat) => chat.name === 'Wapp automation test'
+        )
+        setTimeout(() => {
+            client.sendMessage(
+                myGroup.id._serialized,
+                'Esto se enviara despues de 20 segundos'
+            )
+        }, 20000)
+    })
+})
+
+client.initialize()
