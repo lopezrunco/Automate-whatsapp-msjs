68c3f6a87864fecd549db4248b16991f85d991ac
Damián López <lopezrunco@gmail.com>
2022-05-17 17:16:06 +0530

    Code cleaned. Updated readme.

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const { phonesToSend } = require('./read-excel')
-console.log('phonesToSend: ', phonesToSend)
+const xlsx = require('xlsx')
+const workbook = xlsx.readFile('data/test.xlsx') // Read the excel file
+let worksheet = workbook.Sheets[workbook.SheetNames[0]] // Read the first worksheet
 
 const qrcode = require('qrcode-terminal')
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js')
 const client = new Client({ authStrategy: new LocalAuth() })
 
+let phonesToSend = []
+
+// Loop over the records. Start with 2 because first row are the columns. Read the elements by Cell index
+for (let index = 2; index < 7; index++) {
+    const phone = worksheet[`D${index}`].v
+    phonesToSend.push(phone)
+}
+
+// Generate QR to login
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
 })
 
+// Send messages
 client.on('ready', () => {
     console.log('Client is ready!')
-    const numbers = []
-    const text = "Esto es un mensaje probando la automatizacion de wapp. Tranqui."
+    console.log('Sending to the numbers: ', phonesToSend)
+    const text = "This is a automated wapp message"
 
-    numbers.map(element => {
-        const chatId = element.substring(1) + "@c.us"
+    phonesToSend.map(element => {
+        const chatId = element + "@c.us"
         client.sendMessage(chatId, text);
     })
+    console.log('Done!')
 })
 
 client.initialize()
--- readme.md
+++ readme.md
@@ -1,2 +1,31 @@
-### Automate whatsapp msjs
+## Automate whatsapp messages
+From an Excel file, automate the sending of messages through Whatsapp web.
 
+### Instructions:
+
+Create your .xlsx file with 5 rows. Example:
+```bash
+
+| F Name | L Name    | Date       | Phone      |
+|--------|-----------|------------|------------|
+| Lenny  | Gomez     | 05-12-2020 | 1234567890 |
+| Carl   | Perez     | 12-07-2021 | 0987654321 |
+| Homer  | Rodriguez | 19-08-2021 | 1112223334 |
+| Moe    | Smith     | 01-02-2022 | 5556667778 |
+| Fred   | Rock      | 11-10-2022 | 9990001234 |
+```
+Install dependencies
+```bash
+npm i
+```
+Customize the message
+```bash
+const text = "This is a automated wapp message"
+```
+Start the app
+```bash
+npm start
+```
+Scan the Whatsapp QR
+
+The messages will be sent automatically
